88d03d12d240b79316bf4a23dc5c84770495f654
Jhonatan <jhonatanbds@outlook.com>
2019-11-27 15:39:31 -0300

    final tests

--- crud.hs
+++ crud.hs
@@ -93,7 +93,15 @@ calculaASobra mes ano = do
   d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
   case d of
     Left err -> putStrLn err
-    Right ps -> print (Data.Foldable.sum (pegaTodasAsReceitas mes ano (removeTransacoesNaoConsideradas ps)) + Data.Foldable.sum (pegaTodasAsDespesas mes ano (removeTransacoesNaoConsideradas ps)) )
+    Right ps -> print (Data.Foldable.sum (pegaTodasAsReceitas mes ano (removeTransacoesNaoConsideradas ps)) - Data.Foldable.sum (pegaTodasAsDespesas mes ano (removeTransacoesNaoConsideradas ps)) )
+  
+
+calculaMediaDaSobra :: Int -> IO()
+calculaMediaDaSobra ano = do
+  d <- (eitherDecode <$> getJSON) :: IO (Either String [Transaction])
+  case d of
+    Left err -> putStrLn err
+    Right ps -> print ((Data.Foldable.sum (pegaTodasAsReceitasDoAno ano (removeTransacoesNaoConsideradas ps)) - Data.Foldable.sum (pegaTodasAsDespesasDoAno ano (removeTransacoesNaoConsideradas ps)) ) / (fromIntegral (Prelude.length  (pegaTodasAsDespesasDoAno ano (removeTransacoesNaoConsideradas ps)))))
 
 calculaMaxNoMesAno :: Int -> Int -> IO ()
 calculaMaxNoMesAno mes ano = do
@@ -129,9 +137,6 @@ calculaFluxoDeCaixa mes ano = do
   case d of
     Left err -> putStrLn err
     Right ps -> print (fluxo (filtrarPorMesAno mes ano (removeTransacoesNaoConsideradas ps)))
-
-
-
 
 
 {-- AUXILIARY FUNCTIONS --}
@@ -200,11 +205,114 @@ pegaTodasAsDespesas mes ano dado = [(valor x) | x <- dado, ( year (dataC x)) == 
 pegaTodasAsReceitasDoAno ano dado = [(valor x) | x <- dado, ( year (dataC x)) == ano, ( valor x) > 0 ]
 pegaTodasAsDespesasDoAno ano dado =  [(valor x) | x <- dado, ( year (dataC x)) == ano, ( valor x) < 0 ]
 
+get_texto t = (textoIdentificador t)
+
 main :: IO ()
 main = hspec $ do
     describe "filtrarTransacoesPorAno" $ do
+    it "quantidade de transacoes no ano de 2017" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+            Left err -> putStrLn err
+            Right ps -> Prelude.length [x | x <- ps, (year (dataC x)) == 2017 ]  `shouldBe` 1336
     it "quantidade de transacoes no ano de 2018" $ do
         d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
         case d of
             Left err -> putStrLn err
             Right ps -> Prelude.length [x | x <- ps, (year (dataC x)) == 2018 ]  `shouldBe` 1146
+    it "quantidade de transacoes no ano de 2019" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+            Left err -> putStrLn err
+            Right ps -> Prelude.length [x | x <- ps, (year (dataC x)) == 2019 ]  `shouldBe` 1207
+    describe "filtrarTransacoesPorAnoMes" $ do
+    it "quantidade de transacoes no ano de 2018 no mes 3" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> Prelude.length (filtrarPorMesAno 3 2018 ps) `shouldBe` 96
+        
+    it "quantidade de transacoes no ano de 2019 no mes 1" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> Prelude.length (filtrarPorMesAno 1 2019 ps) `shouldBe` 84
+
+    it "quantidade de transacoes no ano de 2018 no mes 12" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+            Left err -> putStrLn err
+            Right ps -> Prelude.length (filtrarPorMesAno 12 2018 ps) `shouldBe` 0
+    
+    describe "calculaValorDasReceitas" $ do        
+      it "Calcular o valor das receitas (créditos) no ano de 2018 no mes 12" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (Data.Foldable.sum (pegaTodasAsReceitas 12 2018 (removeTransacoesNaoConsideradas ps))) `shouldBe` 0.0
+
+    describe "calculaValorDasDespesas" $ do        
+      it "Calcular o valor das despesas (débitos) ano de 2018 no mes 12" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (Data.Foldable.sum (pegaTodasAsDespesas 12 2018 (removeTransacoesNaoConsideradas ps))) `shouldBe` 0.0
+
+    describe "calculaValorDasSobras" $ do        
+      it "Calcular a sobra (receitas - despesas) no ano de 2018 no mes 12" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (Data.Foldable.sum (pegaTodasAsReceitas 12 2018 (removeTransacoesNaoConsideradas ps)) - Data.Foldable.sum (pegaTodasAsDespesas 12 2018 (removeTransacoesNaoConsideradas ps)) ) `shouldBe` 0.0
+      
+    describe "calculaSaldo" $ do        
+      it "Calcular o saldo final" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        pending
+    
+    describe "calculaSaldoMaximo" $ do        
+      it "Calcular o saldo máximo atingido no ano de 2018 no mes 3" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (get_texto (maxNoDado (filtrarPorMesAno 3 2018 (removeTransacoesNaoConsideradas ps)))) `shouldBe` "CR\201D.LIQ.COBRAN\199A"
+      
+    describe "calculaSaldoMinimo" $ do        
+      it "Calcular o saldo minimo atingido no ano de 2018 no mes 3" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (get_texto (minNoDado (filtrarPorMesAno 3 2018 (removeTransacoesNaoConsideradas ps)))) `shouldBe` "PGS-CH PR\211P COOP/AG"
+      
+    describe "calculaMediaReceitas" $ do        
+      it "Calcular a média das receitas no ano de 2018" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> ((Data.Foldable.sum  (pegaTodasAsReceitasDoAno 2018 (removeTransacoesNaoConsideradas ps))) / (fromIntegral (Prelude.length  (pegaTodasAsReceitasDoAno 2018 (removeTransacoesNaoConsideradas ps))))) `shouldBe` 5415.0576
+      
+    describe "calculaMediaDespesas" $ do        
+      it "Calcular a média das despesas no ano de 2018" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> ((Data.Foldable.sum  (pegaTodasAsDespesasDoAno 2018 (removeTransacoesNaoConsideradas ps))) / (fromIntegral (Prelude.length  (pegaTodasAsDespesasDoAno 2018 (removeTransacoesNaoConsideradas ps))))) `shouldBe` -1428.7981
+      
+    describe "calculaMediaSobras" $ do        
+      it "Calcular a média das sobras" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        pending
+  
+    describe "retornaFluxoDeCaixa" $ do        
+      it "Retornar o fluxo de caixa de determinado mês/ano no ano de 2018 no mes 3" $ do
+        d <- (eitherDecode <$> getJSON) :: IO (Either String [Crud.Transaction])
+        case d of
+          Left err -> putStrLn err
+          Right ps -> (fluxo (filtrarPorMesAno 3 2018 (removeTransacoesNaoConsideradas ps))) `shouldBe` [(2,8.350048),(3,5962.22),(4,-789.92),(5,-23551.18),(6,-15569.9),(9,-1154.0),(10,-13446.461),(11,3908.8801),(12,1360.1399),(13,2607.11),(16,-6976.09),(17,2136.5999),(18,3174.2002),(19,-3577.6802),(20,55066.8),(23,1644.8301),(24,1905.19),(25,88.51997),(26,-14158.721),(27,475.88),(30,304.35)]
+      
+      
+
+
+
+    
+
